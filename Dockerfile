c3f55eddd3acd9e8f95ba3eb5d9b872d48334554
Practicant5 <dik.r.a@martin.su>
2025-06-05 02:29:49 +0300

    Обновлены зависимости в requirements.txt и внесены изменения в настройки проекта: добавлены переменные окружения для SECRET_KEY, DEBUG и ALLOWED_HOSTS, а также настроен dj-database-url для подключения к базе данных. Добавлен middleware Whitenoise для обработки статических файлов.

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+# Используем официальный Python-образ
+FROM python:3.11-slim
+
+# Устанавливаем рабочую директорию
+WORKDIR /app
+
+# Копируем файлы проекта
+COPY . .
+
+# Устанавливаем зависимости
+RUN pip install --upgrade pip && \
+    pip install -r requirements.txt
+
+
+# Открываем порт
+EXPOSE 8000
+
+# Команда запуска
+CMD ["gunicorn", "internet_shop.wsgi", "--bind", "0.0.0.0:8000"] 
--- internet_shop/settings.py
+++ internet_shop/settings.py
@@ -1,16 +1,19 @@
 import os
 from pathlib import Path
+import dj_database_url
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-your-secret-key'
+load_dotenv()
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-your-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 # Application definition
 INSTALLED_APPS = [
@@ -25,6 +28,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -55,10 +59,7 @@ WSGI_APPLICATION = 'internet_shop.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL', f'sqlite:///{BASE_DIR}/db.sqlite3'))
 }
 
 # Password validation
@@ -84,8 +85,9 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
 MEDIA_URL = 'media/'
